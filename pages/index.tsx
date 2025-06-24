364ac87c4d2df69980bb13f814f7b7aaf301c5e4
droppyadmin <droppytome@gmail.com>
2025-06-24 18:48:54 +0200

    Delete pages/index.tsx (migrating to App Router)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,1 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 text-center p-4">
-      <div>
-        <h1 className="text-3xl font-bold mb-4">Üdvözöl a DropTag</h1>
-        <p className="text-gray-600">Ez a nyitóoldal. Későbbiekben itt lehet útválasztás, logó, info stb.</p>
-      </div>
-    </div>
-  );
-}
+
