5147e00b7da3e3c2b06673c0a607fc5b8b6f66b2
droppyadmin <droppytome@gmail.com>
2025-06-25 02:42:21 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,20 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        droppy: {
-          light: '#3ddad7',
-          dark: '#23bdb8',
-          text: '#ffffff',
-        },
+        'droppy-light': '#2dd4bf',     // pl. CTA gomb világos
+        'droppy-dark': '#0f766e',      // pl. sötét módhoz
+        'droppy-text': '#ffffff'       // CTA szöveg, ha kell egyedi
       },
       boxShadow: {
-        droppy: '0 10px 20px rgba(61, 218, 215, 0.3)',
-        droppyDark: '0 10px 20px rgba(35, 189, 184, 0.3)',
-      },
-    },
+        droppy: '0 4px 14px 0 rgba(45, 212, 191, 0.5)',         // világos
+        droppyDark: '0 4px 14px 0 rgba(14, 116, 102, 0.5)'      // sötét módhoz
+      }
+    }
   },
   plugins: [],
 }
